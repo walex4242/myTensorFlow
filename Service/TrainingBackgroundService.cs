27b17db2d1723ca532c61b0debe538cad41bd17e
Olawale Olafisoye <walex.world20@gmail.com>
2025-04-06 21:16:54 -0300

    My first push

---
+++ Service/TrainingBackgroundService.cs
@@ -0,0 +1,20 @@
+﻿
+namespace MyTensorFlowApp.Services
+{
+    public class TrainingBackgroundService(IServiceProvider serviceProvider) : BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider = serviceProvider;
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var model = scope.ServiceProvider.GetRequiredService<SimpleLinearModel>();
+                Console.WriteLine("Starting Simple Linear Regression Training in Background...");
+                await Task.Run(() => model.Train(), stoppingToken);
+                Console.WriteLine("Background training completed.");
+                // Potentially store the trained model somewhere accessible
+            }
+        }
+    }
+}
